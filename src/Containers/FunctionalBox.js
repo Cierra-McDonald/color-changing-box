ef00fbd62acc06e789a890707dfcc8281cb81ed3
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-10 16:47:43 -0700

    components created and color box

---
+++ src/Containers/FunctionalBox.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+const randomColor = require('randomcolor');
+
+function FunctionalBox() {
+    const [color, setColor] = useState();
+
+        return (
+            <div>
+                
+            </div>
+        )
+    
+}
---
+++ src/Presentational/ColorBox.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ColorBox() { 
+    return <div className="colorbox">My Color Box!</div>
+}
+
+export default ColorBox;
