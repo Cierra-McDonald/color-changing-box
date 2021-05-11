c81e0003ee4eb3cb3a044f142281c5927d3f2685
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-11 12:06:13 -0700

    added proptypes to presentational component

--- src/Presentational/ColorBox.js
+++ src/Presentational/ColorBox.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const ColorBox = ({color}) => { 
+const ColorBox = ({color}) => (
+    <div style={{backgroundColor: color, width: '400px', height: '400px', margin: 'auto', marginTop:'8%', borderRadius:'3em', textAlign:'center', justifyContent:'center', border: '3px solid white'}}></div>
+    );
+
     
-    return <div style={{backgroundColor: color, width: '400px', height: '400px', margin: 'auto', marginTop:'8%', borderRadius:'3em', textAlign:'center', justifyContent:'center', border: '3px solid white'}}></div>
+ColorBox.propTypes = { 
+    color: PropTypes.string.isRequired
 }
 
 export default ColorBox;
